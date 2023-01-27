8acfaccf1d5de03669c8a59f739c188c97a71119
Alexsept177 <alan03002@utb.vaxjo.se>
2023-01-27 10:02:02 +0100

    Add project files.

---
+++ Uppgift 5.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Uppgift_5._4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[] { "Alem", "Adam", "Messi", "Ronaldo", "maradona" };
+            Array.Sort(namn);
+            Console.WriteLine(namn[0]);
+        }
+    }
+}
